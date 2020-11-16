83905df28e631e7076c958f046dce99154210e69
Tomos <Willistomos@gmail.com>
2020-11-16 12:29:23 +0000

    Adds Footer

--- wp-content/themes/leeds_events/footer.php
+++ wp-content/themes/leeds_events/footer.php
@@ -11,12 +11,28 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-	
-	</footer><!-- #colophon -->
+	<footer class="w-full bg-gray-200 p-5">
+        <div class="md:w-1/3 pl-5">
+            <h1 class="text-2xl text-orange-400">News and Offers</h1>
+            <p class="font-light">
+                We have exclusive news and offers for the biggest events and incredible performances. You may also save yourself money on food and selected events!
+            </p>
+            <h2 class="font-light text-lg my-2">Email</h2>
+            <input type="text" class="w-full border border-grey-200 border-solid rounded-lg py-2">
+            <div class="l-button bg-orange-400 py-2  text-white w-1/3 text-center rounded-lg text-sm my-2">
+                <p>Get Offers</p> 
+            </div>
+        </div>
+        <div class="md:w-1/3">
+
+        </div>
+        <div class="md:w-1/3">
+
+        </div>
+    </footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<!-- <script src="wp-content/themes/leeds_events/public/assets/js/app.js" async></script> -->
+<script src="wp-content/themes/leeds_events/public/assets/js/app.js" async></script>
 </body>
 </html>
