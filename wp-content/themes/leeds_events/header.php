730ba3dba903b00bf048dfeea7fde9dcbcbbc81d
Tomos <Willistomos@gmail.com>
2020-11-17 09:46:44 +0000

    adds icon styles

--- wp-content/themes/leeds_events/header.php
+++ wp-content/themes/leeds_events/header.php
@@ -16,7 +16,12 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+
 
 	<?php wp_head(); ?>
 </head>
