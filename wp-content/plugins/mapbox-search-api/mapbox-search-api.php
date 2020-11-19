5781fdda53749f40a12637ae8d14f5d5fe9a5cb1
Tomos <Willistomos@gmail.com>
2020-11-19 12:17:29 +0000

    mapbox search init

---
+++ wp-content/plugins/mapbox-search-api/mapbox-search-api.php
@@ -0,0 +1,113 @@
+<?php
+/*
+    Plugin Name: Mapbox Lat & Long Search 
+    description:  If you need to know the lat and long for your mapbox map use this to find it
+   
+    Author: Tomos Willis
+    Version: 1.0.0
+*/
+
+class Smashing_Fields_Plugin {
+
+    public function __construct() {
+        // Hook into the admin menu
+        add_action( 'admin_menu', array( $this, 'create_plugin_settings_page' ) );
+        add_action( 'admin_init', array( $this, 'setup_sections' ) );
+        add_action( 'admin_init', array( $this, 'setup_fields' ) );
+    }
+
+    public function create_plugin_settings_page() {
+        // Add the menu item and page
+        $page_title = 'My Awesome Settings Page';
+        $menu_title = 'Awesome Plugin';
+        $capability = 'manage_options';
+        $slug = 'smashing_fields';
+        $callback = array( $this, 'plugin_settings_page_content' );
+        $icon = 'dashicons-admin-plugins';
+        $position = 100;
+    
+        add_submenu_page( 'options-general.php', $page_title, $menu_title, $capability, $slug, $callback );
+        
+    }
+    
+
+    public function plugin_settings_page_content() { ?>
+        <div class="wrap">
+            <h2>Mapbox Search Settings Page</h2>
+            <form method="post" action="options.php">
+                <?php
+                    settings_fields( 'smashing_fields' );
+                    do_settings_sections( 'smashing_fields' );
+                    submit_button();
+                ?>
+            </form>
+        </div> <?php
+    }
+
+    public function setup_sections() {
+        add_settings_section( 'our_first_section', 'Your Mapbox api Key', array( $this, 'section_callback' ), 'smashing_fields' );
+    }
+
+    public function section_callback( $arguments ) {
+        switch( $arguments['id'] ){
+            case 'our_first_section':
+                echo 'For the search to work you need your mapbox api key. If you have not already got this please visit https://account.mapbox.com/ and create a token';
+                break;
+        }
+    }
+
+
+    public function setup_fields() {
+        $fields = array(
+            array(
+                'uid' => 'our_first_field',
+                'label' => 'Token',
+                'section' => 'our_first_section',
+                'type' => 'text',
+                'options' => false,
+                'placeholder' => 'DD/MM/YYYY',
+                'helper' => 'Does this help?',
+                'supplemental' => 'I am underneath!',
+                'default' => '01/01/2015'
+            )
+        );
+        foreach( $fields as $field ){
+            add_settings_field( $field['uid'], $field['label'], array( $this, 'field_callback' ), 'smashing_fields', $field['section'], $field );
+            register_setting( 'smashing_fields', $field['uid'] );
+        }
+    }
+    
+
+    // public function field_callback( $arguments ) {
+    //     echo '<input name="our_first_field" id="our_first_field" type="text" value="' . get_option( 'our_first_field' ) . '" />';
+    //     register_setting( 'smashing_fields', 'our_first_field' );
+    // }
+
+    public function field_callback( $arguments ) {
+        $value = get_option( $arguments['uid'] ); // Get the current value, if there is one
+        if( ! $value ) { // If no value exists
+            $value = $arguments['default']; // Set to our default
+        }
+    
+        // Check which type of field we want
+        switch( $arguments['type'] ){
+            case 'text': // If it is a text field
+                printf( '<input name="%1$s" id="%1$s" type="%2$s" placeholder="%3$s" value="%4$s" />', $arguments['uid'], $arguments['type'], $arguments['placeholder'], $value );
+                break;
+        }
+    
+        // If there is help text
+        if( $helper = $arguments['helper'] ){
+            printf( '<span class="helper"> %s</span>', $helper ); // Show it
+        }
+    
+        // If there is supplemental text
+        if( $supplimental = $arguments['supplemental'] ){
+            printf( '<p class="description">%s</p>', $supplimental ); // Show it
+        }
+    }
+
+    // // Our code will go here
+};
+new Smashing_Fields_Plugin();
+
